5bfbb95dc6538ad5f3f44ce0e4812e55d09c8a74
yeonseocho <103731501+yeonseocho@users.noreply.github.com>
2023-04-05 11:00:00 +0000

    20220607

--- chp.03/3.3.py
+++ chp.03/3.3.py
@@ -106,8 +106,26 @@ print('num1: ',num1)
 num2=range(1,10)
 print('num2: ', num2)
 num3=range(1,10,2)
+print('num3', num3)
 
+#2)range 객체 활용
+for n in num1:
+    print(n, end=' ')
+print()
+for n in num2:
+    print(n, end='')
+print()
+for n in num3:
+    print(n, end=' ')
 
+#3.3.6 for&list
+#1)list에 자료 저장하기
+lst=[]
+for i in range(10):
+    r=random.randint(1,10)
+    lst.append(r)
+print('lst=',lst)
 
-
-
+#2)list에 자료 참조하기
+for i in range(10):
+    print(lst[i]*0.25)
